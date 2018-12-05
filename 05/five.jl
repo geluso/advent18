a95425154c4caec6663f9dd8eae29d4e67d956c5
Steve Geluso <stevegeluso@gmail.com>
2018-12-04 22:28:59 -0800

    05 part 2

--- 05/five.jl
+++ 05/five.jl
@@ -39,16 +39,9 @@ function is_reactive(n1, n2)
   end
 end
 
-
-# for pair in partition(records, 2)
-open(ARGS[1]) do file
-  println("start")
-  line = strip(readline(file)) 
-  println(line)
-  println(length(line))
+function build_linked_polymer(line)
+  # reverse the line (this was from before doubly-linking
   line = reverse(line)
-  println(line)
-  println(length(line))
 
   last = ListNode(line[1], nothing, nothing)
   for letter in line[2:end]
@@ -57,45 +50,53 @@ open(ARGS[1]) do file
     last = node
   end
   list = LinkedList(last)
+end
 
-  println(myprint(list.root))
-  println(length(myprint(list.root)))
-
+function react_polymer!(list)
   current = list.root
   while current != nothing && current.next != nothing
-    #println("current: ", current.data)
     if is_reactive(list.root, list.root.next)
       list.root.next.next.prev = nothing
       list.root = list.root.next.next
       current = list.root
-      #println("front reacted!")
-      #println(myprint(list.root))
-      #println()
     elseif is_reactive(current.prev, current)
       current.next.prev = current.prev.prev
       current = current.prev.prev
       current.next = current.next.next.next
-      #println("prev reacted!")
-      #println(myprint(list.root))
-      #println()
     elseif is_reactive(current, current.next)
       if current.next.next != nothing
         current.next.next.prev = current.prev
       end
       current = current.prev
       current.next = current.next.next.next
-      #println("curr reacted!")
-      #println(myprint(list.root))
-      #println()
     else
       current = current.next
-      #println("no reaction")
-      #println(myprint(list.root))
-      #println()
     end
   end
+  return list
+end
 
-  println(myprint(list.root))
-  println(length(myprint(list.root)))
+function remove_polymer(line, letter)
+  isNotLetter = c -> c != uppercase(letter) && c != lowercase(letter)
+  return filter(isNotLetter, line)
+end
+
+open(ARGS[1]) do file
+  line = strip(readline(file)) 
+  best = nothing
+
+  alpha = "qwertyuiopasdfghjklzxcvbnm"
+  for letter in alpha
+    curated_line = remove_polymer(line, letter)
+
+    list = build_linked_polymer(curated_line)
+    processed = react_polymer!(list)
+    score = length(myprint(processed.root))
+    if best == nothing || score < best
+      best = score
+      println("better: ", best)
+    end
+  end
+  println("best: ", best)
 end
 
